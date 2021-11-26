2dda48f89ea1669103184e816aca998f23c6ea43
Ranto Rabarisoa <ranto.rabarisoa@gmail.com>
2021-11-26 12:55:05 +0300

    add App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";  // requires a loader 
-import EspaceFlow from "./components/espaceflow/EspaceFlow";
 import Home from "./components/home/Home";
 import { Album } from "./components/album/Album.jsx";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./App.css";
-import Lyrics from "./lyrics/Lyrics";
 import AlbumPage from "./components/albumpage/AlbumPage";
 
 const App = () => {
@@ -15,9 +13,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/espaceflow" element={<EspaceFlow />} />
           <Route path="/Album" element={<Album />} />
-          <Route exact path="/Lyrics" element={<Lyrics />} />
           <Route path="/album/:artist/:album" element={<AlbumPage />}/>
         </Routes>
       </Router>
