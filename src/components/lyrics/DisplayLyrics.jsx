814736f4a462c668dc1715dd7a66eba245a1c457
Ranto Rabarisoa <ranto.rabarisoa@gmail.com>
2021-11-26 10:55:27 +0300

    add search bar suggestion Song

--- src/components/lyrics/DisplayLyrics.jsx
+++ src/components/lyrics/DisplayLyrics.jsx
@@ -1,7 +1,6 @@
 import Lyrics from "./Lyrics"
 
 export const DisplayLyrics = ({lyrics}) => {
-    console.log(lyrics)
     return (
         <div>
             <h1> Title song </h1>
--- src/components/suggestionsong/SuggestionSong.jsx
+++ src/components/suggestionsong/SuggestionSong.jsx
@@ -1,11 +1,32 @@
+import axios from 'axios';
 import React from 'react'
 
-const SuggestionSong = () => {
+const SuggestionSong = (props) => {
+
+    const [searchSong, setSearchSong] = useState("");
+
+    const getArtistSong = async () => {
+        axios.get("http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=7f8f8f5d046ed8eb2174ac55fadb58ae&format=json")
+            .then(response => console.log(response))
+
+
+
+    const handleSearchTerm = (e) => {
+        setSearchSong(e.target.value)
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        props.onSubmit(searchSong)
+    
+
     return (
         <div>
-            
+            <form className="search-bar" onSubmit={handleSubmit}>
+            <input type="text" id="search-bar"placeholder="Search an other song..." onChange={handleSearchTerm} value={searchSong}/>
+            </form>
         </div>
     )
 }
 
-export default SuggestionSong
+export default SuggestionSong;
