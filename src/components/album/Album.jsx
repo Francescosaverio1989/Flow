b738e8f27ef6b0f75f988ce168f4532c4e8861a4
Tadkozh <tad.kozh.bihan@free.fr>
2021-11-26 10:48:02 +0100

    Link to home + securing url

--- src/components/album/Album.jsx
+++ src/components/album/Album.jsx
@@ -1,9 +1,9 @@
 
 import { useEffect, useState } from 'react';
-import { useParams } from "react-router-dom";
+import { Link } from 'react-router-dom'
 import axios from 'axios';
 import './Album.css';
-import { Singles } from "./Singles.jsx";
+import logo from '../../assets/images/logo-flow.png'
 
 export const Album = ({artist, album, setChosenSingle, handleSingleChoice}) => {
 
@@ -30,6 +30,8 @@ export const Album = ({artist, album, setChosenSingle, handleSingleChoice}) => {
 
     return (
       <div>
+        <Link to={`/`}><img src={logo} className="header-album-logo" alt="Logo flow" /></Link>
+        
         { chosenAlbum &&
           <>
             <div className="header-album-container">
@@ -45,7 +47,7 @@ export const Album = ({artist, album, setChosenSingle, handleSingleChoice}) => {
                 {/* <p className ="header-album-wiki">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
                 <p className ="header-album-wiki">Adipisci est quis amet incidunt laborum dignissimos odio molestiae autem, dolorum architecto repellendus cum modi sit excepturi ipsam dolor facere numquam deserunt.</p> */}
                 {/* released */}
-                <p className ="header-album-Released">Released : 202-01-10</p>
+                {/* <p className ="header-album-Released">Released : 2021-01-10</p> */}
 
               </div>  
             </div>  
